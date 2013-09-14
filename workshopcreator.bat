886a232b3e0a871a328a7c919e0a1cab53d62240
Patrick Kuen <patrick.kuen11@gmail.com>
2013-09-14 19:22:30 +0200

    changed .bat files

--- workshopcreator.bat
+++ workshopcreator.bat
@@ -1,4 +1,4 @@
-"../../../bin/gmad.exe" create -out "D:\Benutzer\Patrick\Dateien\Workshop\PAS.gma" -folder "D:\Benutzer\Patrick\Dateien\Server\Valve-Server\steamapps\common\GarrysModDS\garrysmod\addons\pas"
+"../../../bin/gmad.exe" create -out "D:\Benutzer\Patrick\Dateien\Workshop\PAS.gma" -folder "D:\Benutzer\Patrick\Dateien\Server\Valve-Server\steamapps\common\GarrysModDS\garrysmod\addons\PatchProtect"
 
 
 "../../../bin/gmpublish" create -addon "D:\Benutzer\Patrick\Dateien\Workshop\PAS.gma" -icon "D:\Benutzer\Patrick\Pictures\Workshop\PAS.jpg"
