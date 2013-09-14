07162ed90a5952ba76d57d514f86c932e7c11726
Patrick Kuen <patrick.kuen11@gmail.com>
2013-09-14 11:28:07 +0200

    Added .bat files

---
+++ workshopcreator.bat
@@ -0,0 +1,5 @@
+"../../../bin/gmad.exe" create -out "D:\Benutzer\Patrick\Dateien\Workshop\PAS.gma" -folder "D:\Benutzer\Patrick\Dateien\Server\Valve-Server\steamapps\common\GarrysModDS\garrysmod\addons\pas"
+
+
+"../../../bin/gmpublish" create -addon "D:\Benutzer\Patrick\Dateien\Workshop\PAS.gma" -icon "D:\Benutzer\Patrick\Pictures\Workshop\PAS.jpg"
+PAUSE
---
+++ workshopupdater.bat
@@ -0,0 +1,5 @@
+"../../../bin/gmad.exe" create -out "D:\Benutzer\Patrick\Dateien\Workshop\PAS.gma" -folder "D:\Benutzer\Patrick\Dateien\Server\Valve-Server\steamapps\common\GarrysModDS\garrysmod\addons\PAS"
+
+"../../../bin/gmpublish" update -addon "D:\Benutzer\Patrick\Dateien\Workshop\PAS.gma" -id "174063696" -changes "Fixed an important bug"
+
+PAUSE
