917320c80ba0cc10c1a1b867665e94daa59f6960
Dominik <domii894@gmail.com>
2013-09-14 18:35:05 +0200

    Cleaned up pas.lua

--- lua/pas/pas.lua
+++ lua/pas/pas.lua
@@ -1,17 +1,10 @@
--- These are the default settings. Don't mind changing these.
 PAS = PAS or {}
 
---==============================DO NOT EDIT BELOW THIS POINT===================================
 function PAS.Setup(ply)
 
+	--Props
 	ply.cooldown = 0
 	ply.props = 0
-
-	--Prop
-	ply.d_time = 0
-	ply.r_time = 0
-	ply.t_time = 0
-	--ply.props = 0
 	ply.lastprop = "none"
 	ply.spawnspecial = false
 
@@ -21,33 +14,45 @@ function PAS.Setup(ply)
 	ply.tool_t_time = 0
 	ply.tools = 0
 
-	--ply.propspawning = false
 	ply.usingtoolgun = false
 	ply.blocktool = false
 	ply.count = 0
 	ply.toolprops = {}
+
 end
 hook.Add( "PlayerInitialSpawn", "Setup_PropSpawn", PAS.Setup )
 
 function spamaction(ply)
+
 	local action = tonumber(PAS.Settings["spamaction"])
-		if action == 2 then
-			cleanup.CC_AdminCleanup(ply, "", {} )
-			ply.cleanedup = true
-			PAS.InfoNotify(ply, "Your props have been cleanuped!")
-		elseif action == 3 then
-			ply:Kick("Kicked by PAS: Spammer")
-		elseif action == 4 then
-			local banminutes = tonumber(PAS.Settings["bantime"])
-			ply:Ban(banminutes, "Banned by PAS: Spammer")
-		elseif action == 5 then
-			local concommand = tostring(PAS.Settings["concommand"])
-			concommand = string.Replace(concommand, "<player>", ply:Nick())
-			local commands = string.Explode(" ", concommand)			
-			RunConsoleCommand(commands[1], unpack(commands, 2))
-		end
+
+	if action == 2 then
+
+		cleanup.CC_AdminCleanup(ply, "", {} )
+		ply.cleanedup = true
+		PAS.InfoNotify(ply, "Your props have been cleanuped!")
+
+	elseif action == 3 then
+
+		ply:Kick("Kicked by PAS: Spammer")
+
+	elseif action == 4 then
+
+		local banminutes = tonumber(PAS.Settings["bantime"])
+		ply:Ban(banminutes, "Banned by PAS: Spammer")
+
+	elseif action == 5 then
+
+		local concommand = tostring(PAS.Settings["concommand"])
+		concommand = string.Replace(concommand, "<player>", ply:Nick())
+		local commands = string.Explode(" ", concommand)
+		RunConsoleCommand(commands[1], unpack(commands, 2))
+
+	end
+
 end
 
+--Anti Spam Function
 function PAS.Spawn(ply, type, ent, toolgun)
 
 	--If AntiSpam is deactivated
@@ -75,7 +80,7 @@ function PAS.Spawn(ply, type, ent, toolgun)
 				end
 
 				--Notify Client about Wait-Time
-				PAS.Notify( ply, "Wait: " .. math.Round( CurTime() - ply.cooldown, 1))
+				PAS.Notify( ply, "Wait: " .. math.Round( ply.cooldown - CurTime(), 1))
 
 				--Remove Entity
 				ent:Remove()
@@ -94,55 +99,10 @@ function PAS.Spawn(ply, type, ent, toolgun)
 
 	end
 
---[[
-	--Removing Entity
-	if not toolgun then
-
-		if CurTime() < ply.d_time then
-
-			if ply:IsAdmin() and tobool(PAS.Settings["noantiadmin"]) then
-			else
-				timer.Create(ply:Nick().."_propcooldown", 10, 1, function()
-					ply.props = 0
-				end)
-
-				ply.props = ply.props + 1
-		
-				if ply.props >= tonumber(PAS.Settings["spamcount"]) then
-
-					PAS.AdminNotify("Player "..ply:Nick().." is spamming!")
-					ply.props = 0
-					spamaction(ply)
-
-				end
-
-				ply.r_time = ply.d_time - CurTime()
-				PAS.Notify(ply, "Wait: ".. math.Round(ply.r_time,1) .."s")
-
-				--Remove Entity
-				ent:Remove()
-
-				--ply.propspawning = false
-
-				return
-
-			end
-		end
-
-		ply.d_time = CurTime() + tonumber(PAS.Settings["cooldown"])
-
-		--ply.propspawning = false
-
-	else
-
-		ply.count = ply.count + 1
-		ply.toolprops[ply.count] = ent
-
-	end
-]]
 end
 
 local function removeent(ply)
+
 	for i = 1, table.Count(ply.toolprops) do
 
 		if ply.toolprops[i]:IsValid() then ply.toolprops[i]:Remove() end
@@ -162,27 +122,6 @@ function firedToolGun(ply, tr, tool)
 
 	--If activated
 	ply.toolprops = {}
-	--[[
-	if not ply.blocktool then return end
-	if CurTime() < ply.tool_d_time then
-		if ply:IsAdmin() and tobool(PAS.Settings["noantiadmin"]) then
-		else
-			timer.Create(ply:Nick().."_toolcooldown", 10, 1, function()
-				ply.tools = 0
-			end)
-			ply.tools = ply.tools + 1
-			if ply.tools >= tonumber(PAS.Settings["spamcount"]) then
-				PAS.AdminNotify("Player "..ply:Nick().." is spamming with " .. tool .. "s!")
-				ply.tools = 0
-				spamaction(ply)
-			end
-			ply.tool_r_time = ply.tool_d_time - CurTime()
-			PAS.Notify(ply, "Wait: "..math.Round(ply.tool_r_time,1).."s")
-			return false
-		end
-	end
-	ply.tool_d_time = CurTime() + tonumber(PAS.Settings["cooldown"])
-	]]
 	
 	if table.Count(ply.toolprops) == 0 then return end
 
@@ -228,8 +167,6 @@ if cleanup then
 				NADMOD.PlayerMakePropOwner(ply, ent)
 			end
 		end
-
-		--ply.propspawning = true
 
 		if Type != "duplicates" and !ply.usingtoolgun then
 
