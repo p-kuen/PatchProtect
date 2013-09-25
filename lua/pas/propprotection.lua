b31fd7d5a4386354aa4d63a55143718b33f7c3ad
Dominik <domii894@gmail.com>
2013-09-25 22:47:33 +0200

    well...
    :O sry

--- lua/pas/propprotection.lua
+++ lua/pas/propprotection.lua
@@ -2,7 +2,7 @@
 
 function CheckPlayer(ply, ent)
 
-	if !Entity:IsWorld() or ply:IsAdmin() then
+	if !ent:IsWorld() or ply:IsAdmin() then
 
 		if ent.name == ply:Nick() and ent.name != nil then
 			
