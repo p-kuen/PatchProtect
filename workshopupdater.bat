d99943f19f28a37ac50116fba7958785f4740866
Domii894 <domii894@gmail.com>
2014-12-07 12:37:28 +0100

    Updated workshopupdater.bat

--- workshopupdater.bat
+++ workshopupdater.bat
@@ -1,5 +1,5 @@
+@echo off
+set /p changes="Changes: "
 "../../../bin/gmad.exe" create -out "D:\Daten\Workshop\PProtect.gma" -folder "D:\Daten\Server\Valve Server\steamapps\common\GarrysModDS\garrysmod\addons\PatchProtect"
-
-"../../../bin/gmpublish" update -addon "D:\Daten\Workshop\PProtect.gma" -id "183047564" -changes "UPDATE INFOS WILL SHOW UP HERE SOON"
-
+"../../../bin/gmpublish" update -addon "D:\Daten\Workshop\PProtect.gma" -id "183047564" -changes %changes%
 PAUSE
