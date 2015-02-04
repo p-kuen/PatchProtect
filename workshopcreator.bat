0d35b81d57bedd485ccb34aa5258aa188084ce23
Patcher56 <patrick.kuen11@gmail.com>
2015-02-04 15:42:50 +0100

    remove bat files

--- workshopcreator.bat
+++
@@ -1,5 +0,0 @@
-"../../../bin/gmad.exe" create -out "D:\Benutzer\Patrick\Dateien\Workshop\PProtect.gma" -folder "D:\Benutzer\Patrick\Dateien\Server\Valve-Server\steamapps\common\GarrysModDS\garrysmod\addons\PatchProtect"
-
-"../../../bin/gmpublish" create -addon "D:\Benutzer\Patrick\Dateien\Workshop\PProtect.gma" -icon "D:\Benutzer\Patrick\Pictures\Workshop\PProtect.jpg"
-
-PAUSE
--- workshopupdater.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set /p changes="Changes: "
-"../../../bin/gmad.exe" create -out "D:\Daten\Workshop\PProtect.gma" -folder "D:\Daten\Server\Valve Server\steamapps\common\GarrysModDS\garrysmod\addons\PatchProtect"
-"../../../bin/gmpublish" update -addon "D:\Daten\Workshop\PProtect.gma" -id "183047564" -changes "%changes%"
-PAUSE
